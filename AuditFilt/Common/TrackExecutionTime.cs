b7d534f6e9ab0051e8f979886c4e526366b102ce
veerendramullapudi <veerendramullapudi@gmail.com>
2015-03-03 14:48:51 +0530

    Custom Action Filter
    Page Navigation Audits Stored In Separate Folder

---
+++ AuditFilt/Common/TrackExecutionTime.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AuditFilt.Common
+{
+    public class TrackExecutionTime:ActionFilterAttribute,IExceptionFilter
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            string message = "\n" + filterContext.ActionDescriptor.ControllerDescriptor.ControllerName + "->" + filterContext.ActionDescriptor.ActionName + "->On action Executing \t" + DateTime.Now.ToString() + "\n";
+            LogExecutionTime(message);
+        }
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            string message = "\n" + filterContext.ActionDescriptor.ControllerDescriptor.ControllerName + "->" + filterContext.ActionDescriptor.ActionName + "->On action Executed \t" + DateTime.Now.ToString() + "\n";
+            LogExecutionTime(message);
+        }
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            string message = "\n" + filterContext.RouteData.Values["controller"].ToString() + "->" + filterContext.RouteData.Values["action"].ToString() + "->On Result Executing \t" + DateTime.Now.ToString() + "\n";
+            LogExecutionTime(message);
+        }
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            string message = "\n" + filterContext.RouteData.Values["controller"].ToString() + "->" + filterContext.RouteData.Values["action"].ToString() + "->On Result Executed \t" + DateTime.Now.ToString() + "\n";
+            LogExecutionTime(message);
+            LogExecutionTime("---------------------------------------------------------------");
+        }
+        public void OnException(ExceptionContext filterContext)
+        {
+            string message = "\n" + filterContext.RouteData.Values["controller"].ToString() + "->" + filterContext.RouteData.Values["action"].ToString() + "->"+filterContext.Exception.Message+ "\t" + DateTime.Now.ToString() + "\n";
+            LogExecutionTime(message);
+            LogExecutionTime("---------------------------------------------------------------");
+        }
+        private void LogExecutionTime(string data)
+        {
+            File.AppendAllText(HttpContext.Current.Server.MapPath("~/Data/Data.txt"), data);
+        }
+    }
+}
---
+++ AuditFilt/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AuditFilt.Common;
+
+namespace AuditFilt.Controllers
+{
+    public class HomeController : Controller
+    {
+        [TrackExecutionTime]
+        public string Index()
+        {
+            return "Index Action Invoked";
+        }
+        [TrackExecutionTime]
+        public string Welcome()
+        {
+            throw new Exception("Exception Occured");
+        }
+    }
+}
